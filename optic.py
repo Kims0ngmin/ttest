97b9c67163f611eaf5e9ec96308eb962e9381090
Kims0ngmin <winwin27@handong.ac.kr>
2024-02-16 12:03:42 +0900

    1

--- optic.py
+++ optic.py
@@ -51,6 +51,7 @@ while True:
 
     mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
 
+
     hsv[..., 0] = ang*180/np.pi/2
     hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
 
